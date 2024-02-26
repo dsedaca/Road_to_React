a661ecaf7287bf4e6baa3125bbfc60ee2661ff1e
Daniel <dsedaca@hotmail.com>
2024-02-26 16:32:50 -0500

    React JSX section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-const title = 'Test';
+function getTitle(title) {
+  return title;
+}
+
+var name = {
+  firstName: 'Daniel',
+  lastName: 'Sedaca',  
+}
 
 function App() {
   return (
     <div>
-      <h1>Hello React</h1>
+      <h1>Hello {getTitle('React')}</h1>
+      <h3>My name is {name.firstName} {name.lastName}</h3>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
   )
 }
-
 
 export default App
